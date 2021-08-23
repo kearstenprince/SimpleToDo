8af36d9fc562cecece1f84c9aca6c063bd121c54
Kearsten Prince <kearstenprince@gmail.com>
2021-08-23 07:31:17 -0700

    Simple ToDo App

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -44,3 +44,4 @@ public class EditActivity extends AppCompatActivity {
 
     }
 }
+
--- app/src/main/java/com/example/simpletodo/itemAdapter.java
+++ app/src/main/java/com/example/simpletodo/itemAdapter.java
@@ -88,3 +88,4 @@ class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
         }
     }
 }
+
